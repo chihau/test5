06d04dfb2a582e34ab23e4c9cb8414dbf5c821b3
Chihau Chau <chihau@usm.cl>
2025-04-09 11:44:31 -0400

    Agrega nueva ruta de Screen 1 a Screen 2

--- lib/main.dart
+++ lib/main.dart
@@ -10,34 +10,49 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hola Routes - Screen 1'),
+        theme: ThemeData(
+          useMaterial3: false,
+          primarySwatch: Colors.blue,
         ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Ingresa tu nombre',
-                  ),
-                  style: TextStyle(fontSize: 25),
-                  textAlign: TextAlign.center,
+        home: Screen1());
+  }
+}
+
+class Screen1 extends StatelessWidget {
+  const Screen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hola Routes - Screen 1'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Ingresa tu nombre',
                 ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Ir a Screen2'),
-                ),
-              ],
-            ),
+                style: TextStyle(fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Screen2(),
+                    ),
+                  );
+                },
+                child: Text('Ir a Screen2'),
+              ),
+            ],
           ),
         ),
       ),
