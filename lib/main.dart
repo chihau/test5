1d2acab20d6839b5438d348a15d3ef9e2032adf3
Chihau Chau <chihau@usm.cl>
2025-04-09 10:44:23 -0400

    Agrega campo para ingresar el nombre

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,22 @@ class MainApp extends StatelessWidget {
           title: Text('Hola Routes - Screen 1'),
         ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Ingresa tu nombre',
+                  ),
+                  style: TextStyle(fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
